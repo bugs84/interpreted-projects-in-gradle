36bb8ca91571224895cf52ae47af54bd19b8e773
j.vondrous <j.vondrous@localhost>
2019-05-20 16:24:43 +0200

    build.gradle.kts  cleanup

--- interpreted-kotlin-project/build.gradle.kts
+++ interpreted-kotlin-project/build.gradle.kts
@@ -1,45 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+group = "cz.vondr.gradle"
+version = "1.0-SNAPSHOT"
 
-// KOTLIN AND AUTO COMPILE SETUP -------------------------------------
+// KOTLIN SETUP -------------------------------------
 plugins {
     kotlin("jvm") version "1.3.31"
 }
 
-group = "cz.vondr.gradle"
-version = "1.0-SNAPSHOT"
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
 
 repositories {
     mavenCentral()
 }
 
-//dependencies {
-//	compile(kotlin("stdlib"))
-//}
-
-//tasks.withType<KotlinCompile> {
-//	kotlinOptions {
-//		jvmTarget = "1.8"
-//	}
-//}
-//
-//tasks.withType<KotlinTestCompile> {
-//	kotlinOptions {
-//		jvmTarget = "1.8"
-//	}
-//}
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
 }
 
 // PROJECT SPECIFIC SETUP -------------------------
@@ -53,7 +33,7 @@ task("helloKotlin", JavaExec::class) {
     main = "cz.vondr.gradle.HelloKotlinKt"
     classpath = sourceSets["main"].runtimeClasspath
 
-    //To start with debugger - set 'true'
+    // To start with debugger => set 'true'
     debug = false
 }
 
